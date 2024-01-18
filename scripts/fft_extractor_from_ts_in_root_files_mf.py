95f2c342e347280134763c721ff7bf029ec30e7c
Razu Mohiuddin <mohiuddinrazu@gmail.com>
2024-01-18 12:59:31 -0500

    scripts added
    scripts for matched filter vs ML analysis

---
+++ scripts/fft_extractor_from_ts_in_root_files_mf.py
@@ -0,0 +1,108 @@
+import os
+import uproot
+import numpy as np
+import pandas as pd
+import scipy
+from scipy.fft import rfft, rfftfreq
+import pyfftw
+
+
+def fastFT(timeseries):
+    #returns fft, max value in fft, x_bins --> all array_like
+    
+    return pyfftw.interfaces.scipy_fftpack.rfft(timeseries, n=len(timeseries)), np.amax(pyfftw.interfaces.scipy_fftpack.rfft(timeseries, n=len(timeseries))), [i for i in range(0, len(timeseries))]
+
+
+# function to add noise to a given array
+def generate_signal(template, noise_level=0.02):
+    noise = noise_level * np.random.randn(len(template))
+    return template + noise
+###########################################################################################
+# function to collect timeseries data from a single window and adding noise to the signal
+signal_window_start_from = 0
+num_signal_windows = 48
+noise_window_start_from = 100
+num_noise_windows = 195
+###########################################################################################
+
+
+
+def hist_collector(root_file):
+    try:
+        data = []
+        for i in range(signal_window_start_from, signal_window_start_from+num_signal_windows):
+            hist_TS = root_file[f'histTSReal_{i}_0;1'].to_numpy()
+            timeseries = generate_signal(hist_TS[0])
+            fft_ts,_,__ = fastFT(timeseries)
+            data.append(abs(fft_ts)/abs(_))
+        return data
+    
+    except Exception as e:
+        print(e)
+        return []
+# function to collect all zeros from a single window (to generate noise only data)        
+def noise_collector(root_file):
+    try:
+        data = []
+        for i in range(noise_window_start_from, noise_window_start_from+num_noise_windows):
+            hist_TS = root_file[f'histTSReal_{i}_0;1'].to_numpy()
+            timeseries = generate_signal(hist_TS[0])
+            fft_ts,_,__ = fastFT(timeseries)
+            data.append(abs(fft_ts)/abs(_))
+        return data
+    
+    except Exception as e:
+        print(e)
+        return []
+
+def main():
+    path_to_root_files = '/gpfs/gibbs/project/heeger/rm2498/data/Simulation/Cavity/root_files_by_param/'
+    root_file_names = os.listdir(path_to_root_files) # grab all files from above directory
+
+    checkpoint = 0 # For checking progress after certain number of operations
+
+    
+    # collecting signal data
+    for root_file_name in root_file_names:
+        file_path = path_to_root_files + root_file_name
+        root_file = uproot.open(file_path)
+        print(f"Working on file {file_path}")
+        checkpoint += 1 # checkpoint
+
+        data = hist_collector(root_file)
+        if data:
+            save_data_to_csv(np.array(data))
+            save_label_to_csv(np.array([1 for i in range(signal_window_start_from, num_signal_windows+signal_window_start_from)])) # labeling all windows with 1's
+            
+            if checkpoint%5 == 0: print(data) # Print current root file info with label
+   
+    # add noise only data to the end
+    for root_file_name in root_file_names:
+        file_path = path_to_root_files + root_file_name
+        root_file = uproot.open(file_path)
+        print(f"Working on file {file_path}")
+        checkpoint += 1 # checkpoint
+
+        data = noise_collector(root_file)
+        if data:
+            save_data_to_csv(np.array(data))
+            save_label_to_csv(np.array([0 for i in range(noise_window_start_from, noise_window_start_from+num_noise_windows)])) # labeling all windows with 0's
+            
+            if checkpoint%5 == 0: print(data) # Print current root file info with label            
+
+# function for saving data every step (step = root file). Make sure file_name already doesn't exist
+def save_data_to_csv(data, file_name="89_degree_electron_tracks_20_80_1.csv"):
+    data = np.array(data)
+    df = pd.DataFrame(data)
+    df.to_csv(file_name, header=None, index=None, mode="a")
+
+# function to write true/false label in a seprate file. Make sure file_name already doesn't exist
+def save_label_to_csv(data, file_name="label_89_degree_electron_tracks_20_80_1.csv"):
+    data = np.array(data)
+    df = pd.DataFrame(data)
+    df.to_csv(file_name, header=None, index=None, mode="a")
+
+if __name__ == "__main__":
+    main()
+
+
